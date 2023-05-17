5108ac721c387f0606c907d9b51f4d63d3b950cb
André Manoel Albani Cezar <andre.11952@sesc-sc.com.br>
2023-05-17 19:06:01 -0300

    desenvolvendo calculadora

---
+++ calculadora/src/components/Calculadora.js
@@ -0,0 +1,33 @@
+
+import './Calculadora.css';
+
+
+
+function Calculadora() {
+  return (
+    <div className="Calculadora">
+    <div>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    </div>
+    <div>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    </div>
+    <div>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    </div>
+    <div>
+    <button>c</button>
+    <button>0</button>
+    <button>,</button>
+    </div>
+    </div>
+  );
+}
+
+export default Calculadora;
